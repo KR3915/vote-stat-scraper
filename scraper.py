8330b974b378bdfd60f1421d0fc5742a2a58f466
KR3915 <ondrej.brdek@gmail.com>
2025-06-01 21:15:10 +0200

    classes init

--- scraper.py
+++ scraper.py
@@ -1,3 +1,19 @@
 import re
-import requests
-#test
+from bs4 import BeautifulSoup as bs
+
+class Obce:
+    def __init__(self, name, kod, url):
+        self.name = name
+        self.kod = kod
+        self.url = url
+    def getdata(url):
+        pass
+
+class strana:
+    def __init__(self, name, pocet, hlasu ):
+        pass
+
+class vysledkystrany:
+    def __init__(self, obec, strana, hlasy):
+        pass
+        
